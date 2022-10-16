925689b313676046bcf6f22160bd83b9496db23e
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-10-17 01:01:41 +0300

    Backend => adding get offer api to offers controller

--- backend/controllers/offers.controller.js
+++ backend/controllers/offers.controller.js
@@ -4,3 +4,11 @@ const getOffers = async (req, res) => {
   const offers = await Offer.find().populate("company").lean().exec();
   res.json(offers);
 };
+
+const getOffer = async (req, res) => {
+  const offer = await Offer.findById(req.params.id)
+    .populate("company")
+    .lean()
+    .exec();
+  res.json(offer);
+};
