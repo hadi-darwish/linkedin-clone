ccb943a73a55649ca13b6049e32ab887526b1957
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-10-17 01:11:08 +0300

    Backend => adding offers prefix in index.js

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,9 @@ app.use("/auth", authRoutes);
 const usersRoutes = require("./routes/users.routes");
 app.use("/users", usersRoutes);
 
+const offersRoutes = require("./routes/offers.routes");
+app.use("/offers", offersRoutes);
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
