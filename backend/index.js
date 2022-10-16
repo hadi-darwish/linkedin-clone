f7243989b6745eba506cf8364172ab7d444bc90c
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-10-16 17:25:16 +0300

    Backend => creating server

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+require("dotenv").config();
+require("./config/db.config");
+const app = express();
+app.use(express.json());
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
