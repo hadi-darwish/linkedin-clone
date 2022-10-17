edce825a1a2f68d242bef758d4bb5624f1178a30
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-10-17 04:02:40 +0300

    Backend => updating user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -57,6 +57,20 @@ const userSchema = new mongoose.Schema({
       type: String,
     },
   },
+
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
 });
 
 const User = mongoose.model("User", userSchema);
