492d13b856890558e545a4aea32e3d9fe4beb65b
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-10-22 02:12:45 +0300

    Frontend => adding landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Button from "./components/Button/Button";
-import Input from "./components/Input/Input";
-import SignUp from "./components/SignUp/SignUp";
 import React from "react";
-import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
-import Login from "./components/Login/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="*" element={<LandingPage />} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import Login from "../components/Login/Login";
 import SignUp from "../components/SignUp/SignUp";
 import request from "../config/axios";
 
 const LandingPage = () => {
   return (
     <>
-      <SignUp />
+      <Routes>
+        {" "}
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </>
   );
 };
