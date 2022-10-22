c94d70294838c8c6c230d2063892175ca37088d9
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-10-22 03:50:44 +0300

    Frontend => styling job card component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import NavBar from "./components/NavBar/NavBar";
+import JobCard from "./components/JobCard/JobCard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <div className="App">
           <NavBar />
+          <JobCard />
           <Routes>
             <Route path="*" element={<LandingPage />} />
           </Routes>
--- frontend/src/components/JobCard/JobCard.jsx
+++ frontend/src/components/JobCard/JobCard.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import request from "../../config/axios";
 import Button from "../Button/Button";
+import "./JobCard.css";
+import logo from "../../assets/logo.svg";
 
 const JobCard = ({}) => {
   return (
     <>
       <div className="job-card">
         <div className="job-company">
+          <img src={logo} alt="company-logo" />
           <h4>Company Name</h4>
-          <img src="" alt="" />
         </div>
         <div className="job-title">
           <h3>Job Title</h3>
